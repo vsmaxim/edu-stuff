c1a5076855bd6745fd76fc4a9be2e1db42a68f6c
vsmaxim <vsmaxim8@gmail.com>
2018-03-11 13:13:30 +0300

    Добавлена 7 лаба по ОС

---
+++ labs/lab7/sender.cpp
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <semaphore.h>
+#include <mqueue.h>
+#define PTR_SIZE sizeof(int)
+#define QNAME "queue"
+
+struct thread_args {
+    bool thread_run;
+    pthread_t thread_id;
+    thread_args() : thread_run(true) {};
+    int exit_code;
+    mqd_t mq;
+};
+
+static void * thread_func(void *arg) {
+    thread_args *args = (thread_args*) arg;
+    printf("Started first thread\n");
+    char message[] = "Hi!";    
+    while (args->thread_run) {
+        mq_send(args->mq, message, 3, 1);
+        printf("Sent: %s\n", message);
+        sleep(1);
+    }
+    args->exit_code = 1;
+    printf("Finished first thread\n");
+    pthread_exit((void*)&args->exit_code);
+}
+
+int main() {
+    thread_args arg;
+    arg.mq = mq_open(QNAME, O_CREAT, S_IWUSR, NULL);
+    if (pthread_create(&arg.thread_id, nullptr, thread_func, &arg)) {
+        printf("Error opening first thread\n");
+    }
+    getchar();
+    arg.thread_run = false;
+    int *exit_code1;
+    if (pthread_join(arg.thread_id, (void**)&exit_code1)) {
+        printf("Error joining first thread\n");
+    }
+    mq_close(arg.mq);
+    mq_unlink(QNAME);
+    return 0;
+}
